235757b72c843690c5eae592457cbca48abeba8c
Brandi L Mason <masonbrandi66@gmail.com>
2024-01-15 21:31:09 -0700

    added image to mark tile with x

--- tictaclogic.js
+++ tictaclogic.js
@@ -1,4 +1,8 @@
 function handleClickEvent(tile) {
     let position = tile.getAttribute("data-position")
+    let img = document.createElement("img");
+    img.src="x.png"
+    tile.appendChild(img)
+
     console.log("you clicked " + position)
 }
