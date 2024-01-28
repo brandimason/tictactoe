ed99a95398a86084f8802aeb733505ab4f23e6aa
Brandi L Mason <masonbrandi66@gmail.com>
2024-01-27 21:56:20 -0700

    added winning logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 
-function Square({value, onSquareClick}){
+function Square({value, onSquareClick}) {
     return (
         <div className="box" onClick={onSquareClick}>
             {value}
         </div>
     )
-  };
+};
 
 export default function Board() {
     const [xIsNext, setXIsNext] = useState(true)
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
-        if (squares[i]) {
-            console.log("hello")
+        if (squares[i] || calculateWinner(squares)) {
             alert("This space is taken! Please pick a blank space for your next move.")
             return;
         }
@@ -26,6 +25,32 @@ export default function Board() {
         }
         setSquares(nextSquares);
         setXIsNext(!xIsNext)
+    }
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i]
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner
     }
 
     return (
